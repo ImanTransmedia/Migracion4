bea55054dd59a7d9a1a0d7952dc75a9cfc50fe75
DESKTOP-M1A9SLT\Extreme Pro <imantransmedia@gmail.com>
2024-11-06 18:26:52 -0500

    configuracion efecto/funcional sin boton

--- main.js
+++ main.js
@@ -6,12 +6,11 @@ const THREE = window.MINDAR.IMAGE.THREE;
 
 document.addEventListener("DOMContentLoaded", async() => {
   
+  // Configuración de MindAR y Three.js
   const mindarThree = new window.MINDAR.IMAGE.MindARThree({
-      container: document.body,
-      imageTargetSrc:"Target/targets.mind",
-  
-    });
-
+    container: document.body,
+    imageTargetSrc: "Target/targets.mind"
+  });
 
     const { renderer, scene, camera } = mindarThree;
 
@@ -26,39 +25,14 @@ document.addEventListener("DOMContentLoaded", async() => {
     audio.setRefDistance(100);
     // Volumen
     audio.setVolume(9.0);
-    
-    //const startButton = document.getElementById("startButton");
-    //const infoText = document.getElementById("infoText");
-
-    //Oculta o elimina el botón y el texto después de iniciar
-    //startButton.style.display = "none";
-    //infoText.style.display = "none";
 
     const videosData = [
       {
-        url: "https://cdn.glitch.global/5b7a1209-5438-4fcd-96dc-ba81f0837a93/Ar%20Cr%20Plano%2000-MAIN.mp4?v=1702332451877",
-        position: new THREE.Vector3(0, 0, 0),
-      },
-      {
+
         url: "https://cdn.glitch.global/5b7a1209-5438-4fcd-96dc-ba81f0837a93/Ar%20Cr%20Plano%2001-MAIN.mp4?v=1702332455111",
         position: new THREE.Vector3(0, 0, 0.1),
       },
-      {
-        url: "https://cdn.glitch.global/5b7a1209-5438-4fcd-96dc-ba81f0837a93/Ar%20Cr%20Plano%2002-MAIN.mp4?v=1702332455784",
-        position: new THREE.Vector3(0, 0, 0.2),
-      },
-      {
-        url: "https://cdn.glitch.global/5b7a1209-5438-4fcd-96dc-ba81f0837a93/Ar%20Cr%20Plano%2003-MAIN.mp4?v=1702332456514",
-        position: new THREE.Vector3(0, 0, 0.3),
-      },
-      {
-        url: "https://cdn.glitch.global/5b7a1209-5438-4fcd-96dc-ba81f0837a93/Ar%20Cr%20Plano%2004-MAIN.mp4?v=1702332457314",
-        position: new THREE.Vector3(0, 0, 0.4),
-      },
-      {
-        url: "https://cdn.glitch.global/5b7a1209-5438-4fcd-96dc-ba81f0837a93/Ar%20Cr%20Plano%2005-MAIN.mp4?v=1702332458527",
-        position: new THREE.Vector3(0, 0, 0.5),
-      },
+
     ];
 
     const videos = await Promise.all(
@@ -100,14 +74,4 @@ document.addEventListener("DOMContentLoaded", async() => {
     });
 
 
-  /*const startButton = document.createElement("button");
-  startButton.textContent = "Empezar AR";
-  startButton.id = "startButton";
-  startButton.addEventListener("click", start);
-  document.body.appendChild(startButton);
-
-  const infoText = document.createElement("p");
-  infoText.textContent = "Presiona 'Empezar AR' para comenzar";
-  infoText.id = "infoText";
-  document.body.appendChild(infoText);*/
 });
